c3db688201370c357af9b830c5d48c8ea2feb3ac
Carlos Zambrano <cmzambra@espol.edu.ec>
2023-07-18 16:19:50 -0500

    ajustes

--- src/App.js
+++ src/App.js
@@ -7,47 +7,40 @@ import Login from "./pages/Login.js";
 import { AuthProvider } from "./context/AuthContext.js";
 import { ImageProvider } from "./context/ImageContext.js";
 import { OrderProvider } from "./context/OrderContext.js";
-import { useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute.js";
 import { PanelOrders } from "./pages/PanelOrders.js";
 
-
 function App() {
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   const handleBeforeUnload = (event) => {
-  //     event.preventDefault();
-  //     event.returnValue = ""; //En chrome permite activar ventana emergente
-  //     return event.returnValue;
-  //   };
-
-  //   window.addEventListener("beforeunload", handleBeforeUnload);
-
-  //   return () => {
-  //     window.removeEventListener("beforeunload", handleBeforeUnload);
-  //   };
-  // }, [navigate]);
-  return (
-    <AuthProvider>
-      <ImageProvider>
-        <OrderProvider>
-          <div className="px-10 dark:bg-slate-900  dark:text-white">
-            <Routes>
-              <Route path="/register" element={<Register />}></Route>
-              <Route path="/login" element={<Login />}></Route>
-              <Route element={<ProtectedRoute />}>
-                <Route path="/" element={<Upload />}></Route>
-                <Route path="/form" element={<Form />}></Route>
-                <Route path="/resume" element={<Resume />}></Route>
-                <Route path="/client-orders" element={<OrderList />}></Route>
-                <Route path="/admin-orders" element={<PanelOrders />} />
-              </Route>
-            </Routes>
-          </div>
-        </OrderProvider>
-      </ImageProvider>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <ImageProvider>
+                <OrderProvider>
+                    <div className="px-10 dark:bg-slate-900  dark:text-white">
+                        <Routes>
+                            <Route
+                                path="/register"
+                                element={<Register />}></Route>
+                            <Route path="/login" element={<Login />}></Route>
+                            <Route element={<ProtectedRoute />}>
+                                <Route path="/" element={<Upload />}></Route>
+                                <Route path="/form" element={<Form />}></Route>
+                                <Route
+                                    path="/resume"
+                                    element={<Resume />}></Route>
+                                <Route
+                                    path="/client-orders"
+                                    element={<OrderList />}></Route>
+                                <Route
+                                    path="/admin-orders"
+                                    element={<PanelOrders />}
+                                />
+                            </Route>
+                        </Routes>
+                    </div>
+                </OrderProvider>
+            </ImageProvider>
+        </AuthProvider>
+    );
 }
 export default App;
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,26 +1,23 @@
-import React, { useContext, useRef } from "react";
-import { ImagesContext } from "../pages/Upload";
+import React, { useRef } from "react";
 import CardImage from "./CardImage";
 import { useImage } from "../context/ImageContext";
 
+function Grid({ listFiles, formatUrl, deleteFromS3 }) {
+    const { images, setImages } = useImage();
 
-function Grid({ listFiles, formatUrl,deleteFromS3 }) {
-    // const { images, setImages } = useContext(ImagesContext);
-    const {images, setImages} = useImage()
-    
     let dragItem = useRef();
     let dragOverItem = useRef();
     let dragOverItemIndex = useRef();
     let dragItemIndex = useRef();
 
     const dragStart = (e, index) => {
-        dragItem.current = e.target
-        dragItemIndex = index
+        dragItem.current = e.target;
+        dragItemIndex = index;
     };
 
     const dragEnter = (e, index) => {
-        dragOverItem.current = e.target
-        dragOverItemIndex = index
+        dragOverItem.current = e.target;
+        dragOverItemIndex = index;
     };
 
     const drop = () => {
@@ -34,10 +31,9 @@ function Grid({ listFiles, formatUrl,deleteFromS3 }) {
         images2.splice(dragIndex, 1); //Elimina el elemento de su posición actual
         images2.splice(dropIndex, 0, temp); //Agrega el elemento en su posición final
         setImages(images2);
-
     };
 
-    const deleteImage = (index) => {
+    const deleteImage = index => {
         const updatedImages = [...images];
         updatedImages.splice(index, 1); // Elimina la imagen en el índice dado
         setImages(updatedImages);
@@ -47,21 +43,22 @@ function Grid({ listFiles, formatUrl,deleteFromS3 }) {
         <>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {images.map((imageDataURL, index) => (
-                    <div key={index}
-                        onDragStart={(e) => dragStart(e, index)}
-                        onDragEnter={(e) => dragEnter(e, index)}
+                    <div
+                        key={index}
+                        onDragStart={e => dragStart(e, index)}
+                        onDragEnter={e => dragEnter(e, index)}
                         onDragEnd={drop}
-                        draggable
-                    >
-                        <CardImage imageDataURL={imageDataURL} onDelete={deleteImage} index={index} ShowButton={true}></CardImage>
+                        draggable>
+                        <CardImage
+                            imageDataURL={imageDataURL}
+                            onDelete={deleteImage}
+                            index={index}
+                            ShowButton={true}></CardImage>
                     </div>
                 ))}
             </div>
-            
         </>
-    )
+    );
 }
 
-export default Grid
-
-
+export default Grid;
